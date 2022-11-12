231413e1944afd69cf0d603636f2e8e555f5c72d
JungHakJin <jh0407jin@naver.com>
2022-11-12 14:26:57 +0900

    Update camera.py

--- src/1. raspberry_pi/camera.py
+++ src/1. raspberry_pi/camera.py
@@ -1,5 +1,17 @@
 
 
 def capture():
-    print("dd")
+    
+    from picamera import PiCamera
+    from time import sleep
+
+    camera = PiCamera()
+
+    camera.start_preview()
+    sleep(2)
+    camera.capture('image.jpg')
+    camera.stop_preview()
+
     print( "capture" )
+    
+    
