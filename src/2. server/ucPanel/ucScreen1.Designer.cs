2cd6dedc0106b18f8cabadc119c4dc8773f443c0
devsmu <dev.lsh1107@gmail.com>
2022-11-14 17:22:09 +0900

    ucScreen1 화면 조정

--- src/2. server/ucPanel/ucScreen1.Designer.cs
+++ src/2. server/ucPanel/ucScreen1.Designer.cs
@@ -60,11 +60,11 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.SystemColors.Control;
 			this.label1.Location = new System.Drawing.Point(13, 12);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(65, 21);
+			this.label1.Size = new System.Drawing.Size(116, 40);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "24 inch";
 			// 
@@ -80,11 +80,11 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold);
 			this.label2.ForeColor = System.Drawing.SystemColors.Control;
 			this.label2.Location = new System.Drawing.Point(13, 12);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(65, 21);
+			this.label2.Size = new System.Drawing.Size(116, 40);
 			this.label2.TabIndex = 0;
 			this.label2.Text = "27 inch";
 			// 
@@ -100,11 +100,11 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold);
 			this.label3.ForeColor = System.Drawing.SystemColors.Control;
 			this.label3.Location = new System.Drawing.Point(13, 12);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(65, 21);
+			this.label3.Size = new System.Drawing.Size(116, 40);
 			this.label3.TabIndex = 0;
 			this.label3.Text = "32 inch";
 			// 
@@ -120,11 +120,11 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold);
 			this.label4.ForeColor = System.Drawing.SystemColors.Control;
 			this.label4.Location = new System.Drawing.Point(13, 12);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(42, 21);
+			this.label4.Size = new System.Drawing.Size(75, 40);
 			this.label4.TabIndex = 0;
 			this.label4.Text = "양품";
 			// 
@@ -140,11 +140,11 @@
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold);
 			this.label5.ForeColor = System.Drawing.SystemColors.Control;
 			this.label5.Location = new System.Drawing.Point(13, 12);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(92, 21);
+			this.label5.Size = new System.Drawing.Size(167, 40);
 			this.label5.TabIndex = 0;
 			this.label5.Text = "불량 case 1";
 			// 
@@ -160,11 +160,11 @@
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Bold);
 			this.label6.ForeColor = System.Drawing.SystemColors.Control;
 			this.label6.Location = new System.Drawing.Point(13, 12);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(88, 21);
+			this.label6.Size = new System.Drawing.Size(159, 40);
 			this.label6.TabIndex = 0;
 			this.label6.Text = "불량 case2";
 			// 
