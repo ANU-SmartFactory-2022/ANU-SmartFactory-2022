415ce98e9422b22844ee4b7aad9e528a976998fc
devsmu <dev.lsh1107@gmail.com>
2022-11-14 16:06:49 +0900

    ucScreen1 기초 디자인 수정

--- src/2. server/ucPanel/ucScreen1.Designer.cs
+++ src/2. server/ucPanel/ucScreen1.Designer.cs
@@ -28,19 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel5.SuspendLayout();
+			this.panel6.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(28, 33);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 150);
+			this.panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(13, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "24 inch";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Location = new System.Drawing.Point(28, 211);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(200, 150);
+			this.panel2.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.Control;
+			this.label2.Location = new System.Drawing.Point(13, 12);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 21);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "27 inch";
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel3.Controls.Add(this.label3);
+			this.panel3.Location = new System.Drawing.Point(28, 390);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(200, 150);
+			this.panel3.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.Control;
+			this.label3.Location = new System.Drawing.Point(13, 12);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(65, 21);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "32 inch";
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel4.Controls.Add(this.label4);
+			this.panel4.Location = new System.Drawing.Point(541, 33);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(200, 150);
+			this.panel4.TabIndex = 1;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.Control;
+			this.label4.Location = new System.Drawing.Point(13, 12);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(42, 21);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "양품";
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel5.Controls.Add(this.label5);
+			this.panel5.Location = new System.Drawing.Point(541, 211);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(200, 150);
+			this.panel5.TabIndex = 1;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.SystemColors.Control;
+			this.label5.Location = new System.Drawing.Point(13, 12);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(92, 21);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "불량 case 1";
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel6.Controls.Add(this.label6);
+			this.panel6.Location = new System.Drawing.Point(541, 390);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(200, 150);
+			this.panel6.TabIndex = 1;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.SystemColors.Control;
+			this.label6.Location = new System.Drawing.Point(13, 12);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(88, 21);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "불량 case2";
 			// 
 			// ucScreen1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+			this.Controls.Add(this.panel6);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.Name = "ucScreen1";
 			this.Size = new System.Drawing.Size(771, 577);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			this.panel5.ResumeLayout(false);
+			this.panel5.PerformLayout();
+			this.panel6.ResumeLayout(false);
+			this.panel6.PerformLayout();
 			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Label label6;
+	}
 }
