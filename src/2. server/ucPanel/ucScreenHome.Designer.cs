ad01196ae9a97fb8524c5e04ea66de6713f777ee
devsmu <dev.lsh1107@gmail.com>
2022-11-16 15:16:11 +0900

    home 화면 수정

--- src/2. server/ucPanel/ucScreenHome.Designer.cs
+++ src/2. server/ucPanel/ucScreenHome.Designer.cs
@@ -61,7 +61,6 @@
 			this.label2.TabIndex = 1;
 			this.label2.Text = "Smart Factory";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			this.label2.Click += new System.EventHandler(this.label2_Click);
 			// 
 			// timer2
 			// 
--- src/2. server/ucPanel/ucScreenHome.cs
+++ src/2. server/ucPanel/ucScreenHome.cs
@@ -34,9 +34,5 @@ namespace WindowsFormsApp4.ucPanel
 			label3.Text = DateTime.Now.ToString("yyyy-MM-dd");
 		}
 
-		private void label2_Click(object sender, EventArgs e)
-		{
-
-		}
 	}
 }
