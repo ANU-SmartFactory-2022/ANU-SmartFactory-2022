c988c0a4f7a28a7e21ccef3fe693e76924d18991
devsmu <dev.lsh1107@gmail.com>
2022-11-17 13:25:04 +0900

    ucScreen1 버튼 하나로 통일

--- src/2. server/ucPanel/ucScreen1.Designer.cs
+++ src/2. server/ucPanel/ucScreen1.Designer.cs
@@ -46,6 +46,14 @@
 			this.btn_24_2 = new System.Windows.Forms.Button();
 			this.btn_24_3 = new System.Windows.Forms.Button();
 			this.btn_24_4 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
 			this.panel3.SuspendLayout();
@@ -82,6 +90,10 @@
 			// panel2
 			// 
 			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel2.Controls.Add(this.button4);
+			this.panel2.Controls.Add(this.button3);
+			this.panel2.Controls.Add(this.button2);
+			this.panel2.Controls.Add(this.button1);
 			this.panel2.Controls.Add(this.label2);
 			this.panel2.Location = new System.Drawing.Point(28, 211);
 			this.panel2.Name = "panel2";
@@ -102,6 +114,10 @@
 			// panel3
 			// 
 			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+			this.panel3.Controls.Add(this.button8);
+			this.panel3.Controls.Add(this.button7);
+			this.panel3.Controls.Add(this.button6);
+			this.panel3.Controls.Add(this.button5);
 			this.panel3.Controls.Add(this.label3);
 			this.panel3.Location = new System.Drawing.Point(28, 390);
 			this.panel3.Name = "panel3";
@@ -207,7 +223,7 @@
 			this.btn_24_1.TabIndex = 1;
 			this.btn_24_1.Text = "1공정";
 			this.btn_24_1.UseVisualStyleBackColor = true;
-			this.btn_24_1.Click += new System.EventHandler(this.btn_24_1_Click);
+			this.btn_24_1.Click += new System.EventHandler(this.btn_Click);
 			// 
 			// btn_24_2
 			// 
@@ -217,7 +233,7 @@
 			this.btn_24_2.TabIndex = 2;
 			this.btn_24_2.Text = "2공정";
 			this.btn_24_2.UseVisualStyleBackColor = true;
-			this.btn_24_2.Click += new System.EventHandler(this.btn_24_2_Click);
+			this.btn_24_2.Click += new System.EventHandler(this.btn_Click);
 			// 
 			// btn_24_3
 			// 
@@ -227,7 +243,7 @@
 			this.btn_24_3.TabIndex = 3;
 			this.btn_24_3.Text = "3공정";
 			this.btn_24_3.UseVisualStyleBackColor = true;
-			this.btn_24_3.Click += new System.EventHandler(this.btn_24_3_Click);
+			this.btn_24_3.Click += new System.EventHandler(this.btn_Click);
 			// 
 			// btn_24_4
 			// 
@@ -237,7 +253,87 @@
 			this.btn_24_4.TabIndex = 4;
 			this.btn_24_4.Text = "4공정";
 			this.btn_24_4.UseVisualStyleBackColor = true;
-			this.btn_24_4.Click += new System.EventHandler(this.btn_24_4_Click);
+			this.btn_24_4.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(20, 64);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(66, 27);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "1공정";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(107, 64);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(66, 27);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "2공정";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(20, 105);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(66, 27);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "3공정";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(107, 105);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(66, 27);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "4공정";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(20, 66);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(66, 27);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "1공정";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(107, 66);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(66, 27);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "2공정";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(20, 110);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(66, 27);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "3공정";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.btn_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(107, 110);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(66, 27);
+			this.button8.TabIndex = 6;
+			this.button8.Text = "4공정";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.btn_Click);
 			// 
 			// ucScreen1
 			// 
@@ -291,5 +387,13 @@
 		private System.Windows.Forms.Button btn_24_3;
 		private System.Windows.Forms.Button btn_24_2;
 		private System.Windows.Forms.Button btn_24_1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
 	}
 }
