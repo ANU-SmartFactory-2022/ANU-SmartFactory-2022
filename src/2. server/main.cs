cf37056780572eff7f8e19801ee4e29cf7ca6692
Novelmine <110361118+Novelmine@users.noreply.github.com>
2022-11-19 16:46:35 +0900

    오라클 빌드 및 서버추가

--- src/2. server/main.cs
+++ src/2. server/main.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using Oracle.ManagedDataAccess.Client;
+using TCPIP_Sample_CSharp;
 
 namespace WindowsFormsApp4
 {
@@ -32,6 +34,15 @@ namespace WindowsFormsApp4
 
         List<Button> btn_list = new List<Button>();
 
+        //오라클 통신위한 객체 지정 및 설정
+        OracleCommand cmd = new OracleCommand();
+        OracleDataReader rdr;
+        OracleConnection conn = new OracleConnection(strConn);
+        static string strConn = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));User Id=system ;Password=20161268;";
+        OracleDataAdapter adapt = new OracleDataAdapter();
+
+        //서버 클라이언트 설정을 위한 객체 설정
+        socket_server m_server = null;
         public main()
         {
             InitializeComponent();
@@ -47,10 +58,87 @@ namespace WindowsFormsApp4
         private void Form1_Load(object sender, EventArgs e)
         {
             //test---
-            button_click(btn_home, e);
+            conn.Open();
+            cmd.Connection = conn;
+            m_server = new socket_server(recv_callback);
+            m_server.start();
+        }
+       
+        private void recv_callback(string _msg)
+        {
+            checkmsg(_msg);
         }
 
-
+        delegate void StringArgReturningVoidDelegate(string _msg);
+        private void checkmsg(string _msg)
+        {
+            string[] TCPmsg = _msg.Split(',');
+            string P_NUM = "";
+            switch (TCPmsg[0])
+            {
+                case "QR_READING":
+                    // DB 조회 및 인치값 발사하기
+                    P_NUM = TCPmsg[1];
+                    cmd.CommandText = $"select PINCH from PRD WHERE PPdNumber = '{P_NUM}' ";
+                    rdr = cmd.ExecuteReader();
+                    string inch = "";
+                    while (rdr.Read())
+                    {
+                        inch = rdr["PINCH"].ToString();
+                    }
+                    m_server?.send("REQUEST_RESULT," + inch);
+                    break;
+                case "FRACTIONATION_RESULT":
+                    //오라클 정리 DB축적
+                    string ru = "";
+                    if(TCPmsg[1] == "1")
+                    {
+                        ru = "데드";
+                    }
+                    else if(TCPmsg[1] == "2")
+                    {
+                        ru = "정상";
+                    }
+                    else if(TCPmsg[1] == "3")
+                    {
+                        ru = "핫";
+                    }
+                    else if(TCPmsg[1] == "4")
+                    {
+                        ru = "스턱";
+                    }
+                    string date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    cmd.CommandText = $"INSERT INTO TABLE PRM VALUES('{date}','담장자','사원번호','{ru}')";
+                    cmd.ExecuteNonQuery();
+                    m_server?.send("ROLLING");
+                    break;
+                case "ROLLING_END":
+                    cmd.CommandText = "SELECT CUME_DIST() OVER(ORDER BY PRResult) AS ResultSUM FROM PRM WHERE != '정상' ";
+                    rdr = cmd.ExecuteReader();
+                    double num = 0.0;
+                    while (rdr.Read())
+                    {
+                        num = Convert.ToDouble(rdr["ResultSUM"]);
+                        if(num >= 0.05 && rdr["PRResult"].ToString() != "정상")
+                        {
+                            string Result1 = rdr["PRResult"].ToString();
+                            if (Result1 == "핫")
+                            {
+                                //닫는 함수
+                            }
+                            else if (Result1 == "스턱")
+                            {
+                                //닫는 함수
+                            }
+                            else if (Result1 == "데드")
+                            {
+                                //닫는 함수
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
         private void button_click( object sender, EventArgs e )
         {
             Button btn = (Button)sender;
@@ -104,6 +192,16 @@ namespace WindowsFormsApp4
         private void panel_MouseUp(object sender, MouseEventArgs e)
         {
             TagMove = false;
+        }
+
+        private void panel_main_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_title_Paint(object sender, PaintEventArgs e)
+        {
+
         }
 
         private void panel_MouseDown(object sender, MouseEventArgs e)
