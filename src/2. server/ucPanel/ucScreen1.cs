242b4300a219b08161a6709a194e48d46ce46295
devsmu <dev.lsh1107@gmail.com>
2022-11-26 15:28:33 +0900

    ucScreen1 작업 완료

--- src/2. server/ucPanel/ucScreen1.cs
+++ src/2. server/ucPanel/ucScreen1.cs
@@ -19,9 +19,14 @@ namespace WindowsFormsApp4.ucPanel
 
 		private void btn_Click(object sender, EventArgs e)
 		{
+			Button btn = (Button)sender;
+			DialogResult result= MessageBox.Show("해당 공정에 문제가 발생하였습니다. 조치 하시겠습니다?", "공정 오류", MessageBoxButtons.YesNo);
 
-			MessageBox.Show("해당 공정에 문제가 발생하였습니다. 조치 하시겠습니다?", "공정 오류", MessageBoxButtons.YesNo);
-			
+			if(result == DialogResult.Yes)
+			{
+				btn.BackColor= SystemColors.Window;
+			}
+
 		}	
 		
 		//문제가 생긴 공정 버튼을 빨간색으로 표시하는 코드
