5631ba73bfd72a6258059c8b2e265539b0c10a3f
Novelmine <110361118+Novelmine@users.noreply.github.com>
2022-12-10 10:52:17 +0900

    막페이지

--- src/2. server/Login Form.cs
+++ src/2. server/Login Form.cs
@@ -21,7 +21,7 @@ namespace WindowsFormsApp4
                "(HOST=localhost)(PORT=1521)))" +
                "(CONNECT_DATA=(SERVER=DEDICATED)" +
                "(SERVICE_NAME=xe)));" +
-               "User Id=pd68;Password=pd68;";
+               "User Id=hr;Password=hr;";
 
 
         string login_Name;
@@ -42,37 +42,37 @@ namespace WindowsFormsApp4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cmd.CommandText = $"select Mpw from Manager WHERE MId = '{textBox1.Text}'";
-            rdr = cmd.ExecuteReader();
-            string pw = "";
-            while (rdr.Read())
-
-            {
-                pw = rdr["Mpw"].ToString();
-            }
-            if (textBox2.Text == pw)
-
-            {
-                cmd.CommandText = $"select * from Manager WHERE MId = '{textBox1.Text}'";
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    login_Number = rdr["MNumber"].ToString();
-                    login_Name = rdr["MName"].ToString();
-                }
+            //  cmd.CommandText = $"select Mpw from Manager WHERE MId = '{textBox1.Text}'";
+            //  rdr = cmd.ExecuteReader();
+            //  string pw = "";
+            //  while (rdr.Read())
+            //
+            //{
+            //    pw = rdr["Mpw"].ToString();
+            //}
+            //if (textBox2.Text == pw)
+            //
+            //{
+            //    cmd.CommandText = $"select * from Manager WHERE MId = '{textBox1.Text}'";
+            //    rdr = cmd.ExecuteReader();
+            //
+            //    while (rdr.Read())
+            //    {
+            //        login_Number = rdr["MNumber"].ToString();
+            //        login_Name = rdr["MName"].ToString();
+            //    }
 
             this.Hide();
 
-                cmd.CommandText = $"INSERT INTO CON VALUES (TO_CHAR(sysdate,'YYYY-MM-DD HH24:mi:SS'), {login_Number} ,'0')";
-                cmd.ExecuteNonQuery();
+           //     cmd.CommandText = $"INSERT INTO CON VALUES (TO_CHAR(sysdate,'YYYY-MM-DD HH24:mi:SS'), {login_Number} ,'0')";
+             //   cmd.ExecuteNonQuery();
 
                 main1 = new main();
                 main1.login_Name = login_Name;
                 main1.login_Number = login_Number;
                 main1.Show();
 
-            }
+           // }
         }
 
 
