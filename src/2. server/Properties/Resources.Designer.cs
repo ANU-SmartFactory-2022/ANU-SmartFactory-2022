6c8748de1b3dd4d3c7f267a874c65e41742f5633
devsmu <dev.lsh1107@gmail.com>
2022-12-13 14:42:22 +0900

    UI최종

--- src/2. server/Properties/Resources.Designer.cs
+++ src/2. server/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace WindowsFormsApp4.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
-        internal static System.Drawing.Bitmap free_icon_caution_4539472 {
-            get {
-                object obj = ResourceManager.GetObject("free-icon-caution-4539472", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
-        /// </summary>
         internal static System.Drawing.Bitmap free_icon_computer_8281294 {
             get {
                 object obj = ResourceManager.GetObject("free-icon-computer-8281294", resourceCulture);
@@ -113,9 +103,9 @@ namespace WindowsFormsApp4.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
-        internal static System.Drawing.Bitmap free_icon_reload_7324616__1_ {
+        internal static System.Drawing.Bitmap free_icon_reload_7456726 {
             get {
-                object obj = ResourceManager.GetObject("free-icon-reload-7324616 (1)", resourceCulture);
+                object obj = ResourceManager.GetObject("free-icon-reload-7456726", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
